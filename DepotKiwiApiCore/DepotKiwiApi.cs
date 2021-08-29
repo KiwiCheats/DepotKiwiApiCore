ea929afd84628a382cf781202d911b9899eed483
Jon <80230068+AutisticKangaroo@users.noreply.github.com>
2021-08-29 12:37:45 +1200

    make Api class public

--- DepotKiwiApiCore/DepotKiwiApi.cs
+++ DepotKiwiApiCore/DepotKiwiApi.cs
@@ -7,44 +7,44 @@ using DepotKiwiApiCore.RequestModels;
 using DepotKiwiApiCore.Utils;
 
 namespace DepotKiwiApiCore {
-    internal class Api {
-        internal Api(string api) {
+    public class Api {
+        public Api(string api) {
             _requestHelper = new RequestHelper(api);
         }
         
-        internal StatusResponse CreateRepository(string name) {
+        public StatusResponse CreateRepository(string name) {
             return _requestHelper.Post<StatusResponse, DepotCreateRequest>("depot/create", new DepotCreateRequest {
                 Name = name
             });
         }
         
-        internal StatusResponse DeleteRepository(string id) {
+        public StatusResponse DeleteRepository(string id) {
             return _requestHelper.Post<StatusResponse, DepotDeleteRequest>("depot/delete", new DepotDeleteRequest {
                 Id = id
             });
         }
 
-        internal List<DepotListResponse> ListRepositories() {
+        public List<DepotListResponse> ListRepositories() {
             return _requestHelper.Get<List<DepotListResponse>>("depot/list", new());
         }
 
-        internal Depot GetDepot(string id) {
+        public Depot GetDepot(string id) {
             var response = _requestHelper.Get<DepotInfoResponse>($"depot/info/{id}", new());
 
             return response.Success ? response.Depot : null;
         }
 
-        internal byte[] DownloadFile(string depotId, string file) {
+        public byte[] DownloadFile(string depotId, string file) {
             var buffer = _requestHelper.GetRaw($"depot/file/download/{depotId}/{file}", new(), out var status);
 
             return status == HttpStatusCode.OK ? buffer : null;
         }
 
-        internal StatusResponse DeleteFile(string depotId, string file) {
+        public StatusResponse DeleteFile(string depotId, string file) {
             return _requestHelper.Post<StatusResponse>($"depot/file/delete/{depotId}/{file}", new());
         }
 
-        internal StatusResponse UploadFile(string depotId, string name, byte[] buffer) {
+        public StatusResponse UploadFile(string depotId, string name, byte[] buffer) {
             return _requestHelper.Post<StatusResponse, DepotFileUploadRequest>($"depot/file/upload/{depotId}/{name}", new() {
                 Data = Convert.ToBase64String(buffer)
             });
